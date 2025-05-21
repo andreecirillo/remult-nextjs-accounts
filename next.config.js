1347cab37b6ab3b4187c25d69c56e0384a267830
André Cirillo <andrecirillo@hotmail.com>
2025-05-21 09:04:43 -0300

    add secret key

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
   webpack: (config) => {
     config.externals = [ ...config.externals, 'knex', 'sqlite3' ]
     return config;
-  }
+  },
+   reactStrictMode: true,
+  env: {
+    NEXTAUTH_SECRET:"say_carol_love_me_carol_love_me_hey",
+  },
 }
 
 module.exports = nextConfig
